7fd00dd083dfc8f4e95f1e53618fe45a0ec2c2c2
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-03-20 16:20:35 -0500

    Little typo errors solved

--- startup.sh
+++ startup.sh
@@ -3,9 +3,17 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install seclists gobuster ghidra -y
 
+## Setup folders
+mkdir -p ~/HackTheBox/config
+mkdir -p ~/HackTheBox/machines
+mkdir -p ~/HackTheBox/challenges
+mkdir -p ~/GitHub/Youngermaster
+mkdir -p ~/GitHub/AnotherOnes
+
+## Setup finctions
 cd ~
 echo "" >> ~/.bashrc
-cat ./functions/extract_nmap_info.sh
-cat ./functions/fast_ping.sh
-cat ./functions/make_startup_directories.sh
-cat ./functions/nmap_quick_scan.sh
+cat ./functions/extract_nmap_info.sh >> ~/.bashrc
+cat ./functions/fast_ping.sh >> ~/.bashrc
+cat ./functions/make_startup_directories.sh >> ~/.bashrc
+cat ./functions/nmap_quick_scan.sh >> ~/.bashrc
