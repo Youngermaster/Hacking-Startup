7e15a93943d4189d082138dcb03703bdc088d899
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-04-18 15:59:54 -0500

    crackmapexec and smbmap added

--- install_dependencies_arch.sh
+++ install_dependencies_arch.sh
@@ -1,1 +1,1 @@
-sudo pacman -S zaproxy burpsuite seclists gobuster nmap rustscan dirbuster john metasploit exploit-db exploitdb evil-winrm evilginx impacket sambascan
+sudo pacman -S zaproxy burpsuite seclists gobuster nmap rustscan dirbuster john metasploit exploit-db exploitdb evil-winrm evilginx impacket sambascan crackmapexec smbmap
