ed4c796e9df983b7ce79b4a5fc2f04622818c069
Juan Manuel Young Hoyos <36933318+Youngermaster@users.noreply.github.com>
2022-05-31 03:25:01 -0500

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,11 @@
 # Make sure to run as root
 sudo apt-get update -y
+sudo apt-get install seclists gobuster ghidra fping nmap crackmapexec kerbrute grep sed python3-impacket john responder -y
+
+# Python stuff
+sudo apt-get install python3 python3-venv python3-pip -y
+
+# General packages for Python3
+pip3 install matplotlib numpy matplotlib jupyterlab kerbrute usersgenerator ldap3 dnspython future ldapdomaindump
+
 sudo apt-get upgrade -y
-sudo apt-get install seclists gobuster ghidra -y
