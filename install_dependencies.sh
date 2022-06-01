3c210da6f75cc024b865963b3ff14824c002b5ee
Juan Manuel Young Hoyos <36933318+Youngermaster@users.noreply.github.com>
2022-06-01 17:44:29 -0500

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 # Make sure to run as root
 sudo apt-get update -y
-sudo apt-get install seclists gobuster ghidra fping nmap crackmapexec kerbrute grep sed python3-impacket john responder -y
+sudo apt-get install seclists gobuster ghidra fping nmap crackmapexec htop kerbrute grep sed python3-impacket john responder -y
 
 # Python stuff
 sudo apt-get install python3 python3-venv python3-pip -y
