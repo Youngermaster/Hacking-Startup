86157d11352393317c96027a52c37690863a1f18
Juan Manuel Young Hoyos <36933318+Youngermaster@users.noreply.github.com>
2022-08-07 15:21:27 -0500

    Update nmap_port_scanner.sh

--- functions/nmap_port_scanner.sh
+++ functions/nmap_port_scanner.sh
@@ -1,10 +1,8 @@
 # -sCV gets the version and the services that runs on the given ports.
 # -p$(ports) The given ports.
 # -oN exports all the info in a "targeted" mode.
-
-nmap -sCV -p$(ports) $ip -oN targeted
-
 # Note: If the target blocks the pings, use the -Pn flag
 
-nmap -sCV -p$(ports) $ip -oN targeted -Pn
-
+function portScanner() {
+    nmap -sCV -p$2 $1 -oN targeted -Pn
+}
